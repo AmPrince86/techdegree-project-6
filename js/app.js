6adfcbfc610e9e11c7187b5f115ed62a49b3f724
Amber Prince <amber.prince75@yahoo.com>
2021-03-19 09:34:02 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -6,6 +6,11 @@ const letters = document.querySelectorAll('.letter');
 const misses = document.querySelector('.misses');
 let missed = 0;
 
+// #3 click button to hide overlay
+$('.btn__start').on('click', function() {
+    $('#overlay').css('display', 'none');  
+});
+
 // #4 game show phrases go here
 let phrases = [
 'The Avengers',
@@ -15,15 +20,10 @@ let phrases = [
 'Black Panther'
 ]; 
 
-// #3 click button to hide overlay
-$('.btn__start').on('click', function() {
-    $('#overlay').css('display', 'none');  
-});
-
 //#5 return a random phrase from an array
  function getRandomPhraseAsArray(arr) {
     // Get random phrase from array
-    return phrases[Math.floor(Math.random() * arr.length)].split('');
+    return arr[Math.floor(Math.random() * arr.length)].split('');
 }
    randomPhrase = getRandomPhraseAsArray(phrases);
 
@@ -36,25 +36,27 @@ $('.btn__start').on('click', function() {
         } else {
             $('#phrase ul').append('<li class="letter"></li>');
         }
-        return addPhraseToDisplay(phrases);
     }
  }
+         addPhraseToDisplay(randomPhrase);
 
 
 // #7 check if a letter is in the phrase
 const checkletter = (button) => {
     let matched = null;
-    
-    for (i = 0; i < letters.length; i++) {
-        if (button === letters[i].textContent.toLowerCase()) {
-            letters[i].classList.add('show');
+    const letterPhrase = document.querySelectorAll('li');
+    for (i = 0; i < letterPhrase.length; i++) {
+        if (button === letterPhrase[i].textContent.toLowerCase()) {
+            letterPhrase[i].classList.add('show');
             matched = true;
         }
-    };
-
+    }
     return matched;
 };
- //#8 Add an event listener to the
+
+
+
+ //#8 Add an event listener to the keyboard
 keyboard.addEventListener('click', (event) =>  {
     if (event.target.tagName === "BUTTON") {
         event.target.className = 'chosen';
@@ -62,8 +64,10 @@ keyboard.addEventListener('click', (event) =>  {
         const match = checkletter(event.target.textContent.toLowerCase());
         if (match === null) {
             missed++;
-            // Code to change hear icon from liveHeart.png to lostHeart.png would go here
-            //tries.textContent = missed;
+            // #9 Code to change heart icon from liveHeart.png to lostHeart.png would go here
+            let lostHeart = 5 - missed;
+            const heart = document.querySelectorAll('img');
+            heart[lostHeart].setAttribute('src', "images/lostHeart.png")
         }
         //checkWin() function call goes here
         const checkWin = () => {
